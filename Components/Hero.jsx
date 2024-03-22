b7299f668fabd1b56eb24fffd6b04130096f9207
Emon Adi <me@emonadi.com>
2024-03-22 19:11:41 +0600

    Hero section padding incresed

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="pt-[150px] pb-[150px] max-md:pt-[120px] max-md:pb-[120px] text-center">
+    <div className="pt-[150px] pb-[150px] text-center">
       <p className="text-white text-6xl  text-center font-montserrat font-extralight">
         Simplicity. Seamless.{" "}
         <span className="bg-[#0085E9] py-3 px-4 font-bold max-xl:inline-block max-lg:mt-4 text-5xl">
