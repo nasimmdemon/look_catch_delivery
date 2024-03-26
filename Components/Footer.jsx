150f7adcedd1e1cfeb8c712c4721c5b4acef566c
Emon Adi <me@emonadi.com>
2024-03-26 23:18:28 +0600

    Leading done on 24support section

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -3,22 +3,22 @@ const Footer = () => {
     <>
       <div className=" flex justify-center items-center mt-12 pb-12">
         <div className="flex md:border-l-[1px] md:pl-32 max-md:border-r-[1px] max-md:pr-20 flex-col text-white font-poppins font-extralight text-xl">
-          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2">
+          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2 max-md:leading-[12px]">
             Mobile/Web App
           </span>
-          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl  md:p-2">
+          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl max-md:leading-[12px]  md:p-2">
             24/7 Support
           </span>
-          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2">
+          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2 max-md:leading-[12px]">
             Account Managers
           </span>
-          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2">
+          <span className="p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2 max-md:leading-[12px]">
             Budget Control
           </span>
-          <span className=" p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2">
+          <span className=" p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2 max-md:leading-[12px]">
             Staff management
           </span>
-          <span className=" p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2">
+          <span className=" p-4 max-md:pt-2 text-3xl max-md:text-xl md:p-2 max-md:leading-[12px]">
             Trained drivers
           </span>
         </div>
